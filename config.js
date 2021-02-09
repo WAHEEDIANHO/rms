ae53b8d5ba997be1928bec9408cad7bf13d0c984
WAHEEDIANHO <55895023+WAHEEDIANHO@users.noreply.github.com>
2021-02-10 00:08:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   session_name: "session-id",
   session_secret: "1234-7875-6783-9876-5468",
-  url: "mongodb://localhost:27017/SRMS",
+  url: "mongodb+srv://OGIDI:WAHEEDianho206454@cluster0.9kbcc.mongodb.net/test",
 };
